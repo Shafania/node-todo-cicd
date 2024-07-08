9a00b6a4ecd4a421e81902e04e2ca3c010a31d56
Shafania Kouser <157893722+Shafania@users.noreply.github.com>
2024-07-08 15:49:22 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require('express'),
     bodyParser = require('body-parser'),
     // In order to use PUT HTTP verb to edit item
-    methodOverride = requir('method-override'),
+    methodOverride = require('method-override'),
     // Mitigate XSS using sanitizer
     sanitizer = require('sanitizer'),
     app = express(),
